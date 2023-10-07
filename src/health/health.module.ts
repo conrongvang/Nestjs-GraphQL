35f0085878037fda6ef775bdaa85d84ec7e17647
conrongvang <chugaconchaylonton@gmail.com>
2023-10-07 11:33:43 +0700

    add DatabseModule into HealthModule

--- src/health/health.module.ts
+++ src/health/health.module.ts
@@ -1,9 +1,10 @@
 import { Module } from "@nestjs/common";
 import { TerminusModule } from "@nestjs/terminus";
+import { DatabaseModule } from "common/database/database.module";
 import { HealthController } from "./health.controller";
 
 @Module({
-  imports: [TerminusModule],
+  imports: [DatabaseModule, TerminusModule],
   controllers: [HealthController],
 })
 export class HealthModule {}
